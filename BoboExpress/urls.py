5fe8d6f23326fffe3484282a957b69c4b5cba07a
BoboExpress <boboexpress@example.com>
2026-05-24 14:46:07 +0000

    Serve media files on Render

--- BoboExpress/urls.py
+++ BoboExpress/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -58,3 +60,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
